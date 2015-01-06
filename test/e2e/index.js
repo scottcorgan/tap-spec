6cb3db8e5df72c047cecb05e48035bcf497abc46
Khanh.Nguyen <khanh.nguyen@opower.com>
2015-01-06 16:15:53 -0500

    Passing 1 e2e test at a time

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -5,6 +5,7 @@ var test = require('tapes'),
     _ = require('lodash'),
     path = require('path'),
     passTestPath = path.resolve(__dirname, '..', 'fixtures', 'pass-test.txt'),
+    failTestPath = path.resolve(__dirname, '..', 'fixtures', 'fail-test.txt'),
     format = require('chalk'),
     symbols = {
         ok: '\u2713',
@@ -14,6 +15,11 @@ var test = require('tapes'),
 
 
 test('e2e test', function(t) {
+    t.beforeEach(function(t) {
+        // TODO
+        t.end();
+    });
+
     t.test('all-test-pass output', function(t) {
         t.plan(1);
         var testOutStream = fs.createReadStream(passTestPath),
@@ -38,12 +44,40 @@ test('e2e test', function(t) {
         });
     });
 
+    t.test('fail-test output', function(t) {
+        t.plan(1);
+        var testOutStream = fs.createReadStream(failTestPath),
+            actual = '',
+            expected = ' '.repeat(2) + 'beep\n' +
+                ' '.repeat(4) + format.green(symbols.ok) + ' ' + format.gray('should be equal') + '\n' +
+                ' '.repeat(4) + format.red(symbols.err) + ' ' + format.gray('should be equivalent') + '\n' +
+                ' '.repeat(2) + 'boop\n' +
+                ' '.repeat(4) + format.green(symbols.ok) + ' ' + format.gray('should be equal') + '\n' +
+                ' '.repeat(4) + format.green(symbols.ok) + ' ' + format.gray('(unnamed assert)') + '\n' +
+                ' '.repeat(2) + format.red.bold('Failed Tests: ') + 'There was ' + format.red.bold(1) + ' failure\n' +
+                ' '.repeat(4) + '1) beep\n' +
+                ' '.repeat(6) + format.red(symbols.err) + ' ' + format.red('should be equivalent') + '\n' +
+                ' '.repeat(2) + 'total:' + ' '.repeat(5) + '4\n' +
+                format.green(' '.repeat(2) + 'passing:' + ' '.repeat(3) + 3) + '\n' +
+                format.red(' '.repeat(2) + 'failing:' + ' '.repeat(3) + 1);
+
+        testOutStream.pipe(tapSpec);
+        tapSpec.on('data', function(data) {
+            actual += data.toString();
+        });
+
+        testOutStream.on('end', function() {
+            t.deepEqual(normalizeOutput(actual, 0), expected, 'Should output in the right format');
+        });
+    });
+
     t.end();
 });
 
-function normalizeOutput(data) {
+// durationLinePos is the position of 'duration ...' line counting from the last line.
+function normalizeOutput(data, durationLinePos) {
     var noEmptyLine = _.filter(data.split('\n'), function(line) { return line.trim().length !== 0; });
-    noEmptyLine.splice(noEmptyLine.length - 2, 1);      // remove 'duration ...' line
+    noEmptyLine.splice(noEmptyLine.length - durationLinePos - 1, 1);      // remove 'duration ...' line
     return noEmptyLine.join('\n');
 }
 
