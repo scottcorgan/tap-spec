a5529472c883f07345c99cecdade8b6d842f7554
Khanh.Nguyen <khanh.nguyen@opower.com>
2015-01-06 11:18:44 -0500

    Add e2e test

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var test = require('tape'),
+var test = require('tapes'),
     fs = require('fs'),
     _ = require('lodash'),
     path = require('path'),
@@ -14,26 +14,31 @@ var test = require('tape'),
 
 
 test('e2e test', function(t) {
-    t.plan(1);
-    var testOutStream = fs.createReadStream(passTestPath),
-        actual = '',
-        expected = '  beep\n    ' + format.green(symbols.ok) + ' ' + format.gray('should be equal') + '\n' +
-                           '    ' + format.green(symbols.ok) + ' ' + format.gray('should be equivalent') + '\n' +
-                   '  boop\n    ' + format.green(symbols.ok) + ' ' + format.gray('should be equal') + '\n' +
-                           '    ' + format.green(symbols.ok) + ' ' + format.gray('(unnamed assert)') + '\n' +
-                                    '  total:     ' + '4\n' +
-                                    format.green('  passing:   ' + 4) + '\n' +
-                                '  ' + format.green.bold('All tests pass!');
-    testOutStream.pipe(tapSpec);
+    t.test('all-test-pass output', function(t) {
+        t.plan(1);
+        var testOutStream = fs.createReadStream(passTestPath),
+            actual = '',
+            expected = ' '.repeat(2) + 'beep\n' +
+                ' '.repeat(4) + format.green(symbols.ok) + ' ' + format.gray('should be equal') + '\n' +
+                ' '.repeat(4) + format.green(symbols.ok) + ' ' + format.gray('should be equivalent') + '\n' +
+                ' '.repeat(2) + 'boop\n' +
+                ' '.repeat(4) + format.green(symbols.ok) + ' ' + format.gray('should be equal') + '\n' +
+                ' '.repeat(4) + format.green(symbols.ok) + ' ' + format.gray('(unnamed assert)') + '\n' +
+                ' '.repeat(2) + 'total:' + ' '.repeat(5) + '4\n' +
+                format.green(' '.repeat(2) + 'passing:' + ' '.repeat(3) + 4) + '\n' +
+                ' '.repeat(2) + format.green.bold('All tests pass!');
 
-    tapSpec.on('data', function(data) {
-        actual += data.toString();
+        testOutStream.pipe(tapSpec);
+        tapSpec.on('data', function(data) {
+            actual += data.toString();
+        });
+
+        testOutStream.on('end', function() {
+            t.deepEqual(normalizeOutput(actual), expected, 'Should output in the right format');
+        });
     });
 
-    testOutStream.on('end', function() {
-        t.deepEqual(normalizeOutput(actual), expected, 'Should output in the right format');
-    });
-
+    t.end();
 });
 
 function normalizeOutput(data) {
